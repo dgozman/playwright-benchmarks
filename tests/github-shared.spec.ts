7b7d8d780ccec500db627cff1fbf6e6e618f2276
Dmitry Gozman <dgozman@gmail.com>
2024-10-11 09:36:37 +0100

    update tests

--- tests/github-shared.spec.ts
+++ tests/github-shared.spec.ts
@@ -13,13 +13,12 @@ test.afterAll(async () => {
 
 for (let i = 0; i < reps; i++) {
   test('github-shared ' + i, async ({}) => {
-    await page.goto('https://github.com/microsoft');
-    await page.getByRole('link', { name: /Repositories [\d\.]+/ }).click();
-    await page.getByTestId('filter-input').fill('playwright');
-    await page.getByTestId('filter-input').press('Enter');
-    await page.getByRole('link', { name: 'playwright.dev', exact: true }).click();
+    await page.goto('https://github.com/microsoft/playwright.dev');
     await page.getByRole('link', { name: '.gitignore, (File)' }).click();
     await page.getByTestId('latest-commit-html').getByRole('link', { name: '#1083' }).click();
     await expect(page.getByRole('heading', { level: 1 })).toHaveText('chore: update to Docusaurus v3 #1083');
+    await page.getByRole('button', { name: 'edited' }).click();
+    await page.getByRole('menuitem', { name: '@mxschmitt mxschmitt created' }).click();
+    await expect(page.getByRole('article')).toContainText('Build is red, since it requires our stable release to have the </> fix applied.');
   });
 }
--- tests/github.spec.ts
+++ tests/github.spec.ts
@@ -3,13 +3,12 @@ import { reps } from '../common';
 
 for (let i = 0; i < reps; i++) {
   test('github ' + i, async ({ page }) => {
-    await page.goto('https://github.com/microsoft');
-    await page.getByRole('link', { name: /Repositories [\d\.]+/ }).click();
-    await page.getByTestId('filter-input').fill('playwright');
-    await page.getByTestId('filter-input').press('Enter');
-    await page.getByRole('link', { name: 'playwright.dev', exact: true }).click();
+    await page.goto('https://github.com/microsoft/playwright.dev');
     await page.getByRole('link', { name: '.gitignore, (File)' }).click();
     await page.getByTestId('latest-commit-html').getByRole('link', { name: '#1083' }).click();
     await expect(page.getByRole('heading', { level: 1 })).toHaveText('chore: update to Docusaurus v3 #1083');
+    await page.getByRole('button', { name: 'edited' }).click();
+    await page.getByRole('menuitem', { name: '@mxschmitt mxschmitt created' }).click();
+    await expect(page.getByRole('article')).toContainText('Build is red, since it requires our stable release to have the </> fix applied.');
   });
 }
